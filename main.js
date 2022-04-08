d369fa9ad76c9411de28f03ef8c3925d6fc7d198
spanacom <marc@spanacom.com>
2022-04-08 17:55:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,35 @@
 (function() {
-  "use strict";
+    "use strict";
 
-  this.Spanacom = function(apikey) {
+    this.Spanacom = function(apikey) {
 
-    let axiosSpanacom = require('axios'),
-        httpsSpanacom = require('https'),
-        OPTS = {
-          host: 'api.spanads.com',
-          port: 443,
-          prefix: '/api/1.0/',
-          method: 'POST',
-          rejectUnauthorized: false,
-          headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'SpanacomNODEJS/1.0.41'
-          }
+        let axiosSpanacom = require('axios'),
+            httpsSpanacom = require('https');
+
+        this.apikey=apikey;
+        this.debug=false;
+        this.onerror = function(error){
+            console.log('DEFAULT ONERROR:',error);
         };
-    this.apikey=apikey;
-    this.debug=false;
-    this.onerror = function(error){
-      console.log('DEFAULT ONERROR:',error);
+
+        this.api= function(method,params,onresult,onerror) {
+
+
+            params.apikey = this.apikey;
+            if (this.debug) {
+                console.log("Spanacom: Opening request to https://api.spanads.com/api/1.0/" +method + ".json/");
+            }
+            let agent = new httpsSpanacom.Agent({ family: 4 });
+
+            axiosSpanacom.get("https://api.spanads.com/api/1.0/" +method + ".json/", { data: params, httpsAgent: agent })
+                .then(response => {
+                    return onresult(response.data);
+                }).catch(error => {
+                return onerror({
+                    status: 'error',
+                    message:error.message});
+            });
+            //console.log('apikey',apikey,' method',method,' vars',vars);
+        }
     };
-
-    this.api= function(method,params,onresult,onerror) {
-
-
-      params.apikey = this.apikey;
-      if (this.debug) {
-        console.log("Spanacom: Opening request to https://" + OPTS.host + OPTS.prefix +method + ".json/");
-      }
-      let agent = new httpsSpanacom.Agent({ family: 4 });
-
-      axiosSpanacom.get("https://" + OPTS.host + OPTS.prefix +method + ".json/", { data: params, httpsAgent: agent })
-          .then(response => {
-            return onresult(response.data);
-          }).catch(error => {
-        return onerror({
-          status: 'error',
-          message:error.message});
-      });
-      //console.log('apikey',apikey,' method',method,' vars',vars);
-    }
-  };
 }).call(this);
